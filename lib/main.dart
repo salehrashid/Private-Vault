61a3bd440282f748ddad73dadb366a16c5154ea7
salehrashid <saleh.rashid.b@gmail.com>
2026-06-15 21:04:26 +0700

    feat: integrate Shorebird OTA update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart';
 
 import 'app/password_manager_app.dart';
 import 'firebase/firebase_bootstrap.dart';
@@ -7,5 +8,42 @@ import 'firebase/firebase_bootstrap.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FirebaseBootstrap.initialize();
-  runApp(const ProviderScope(child: PasswordManagerApp()));
+  runApp(
+    const ProviderScope(
+      child: ShorebirdUpdateGate(
+        child: PasswordManagerApp(),
+      ),
+    ),
+  );
+}
+
+class ShorebirdUpdateGate extends StatefulWidget {
+  const ShorebirdUpdateGate({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  State<ShorebirdUpdateGate> createState() => _ShorebirdUpdateGateState();
+}
+
+class _ShorebirdUpdateGateState extends State<ShorebirdUpdateGate> {
+  @override
+  void initState() {
+    super.initState();
+    _checkForUpdate();
+  }
+
+  Future<void> _checkForUpdate() async {
+    final updater = ShorebirdUpdater();
+    final status = await updater.checkForUpdate();
+    if (status == UpdateStatus.outdated) {
+      await updater.update();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
 }
